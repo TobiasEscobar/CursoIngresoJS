0231d7c948dd02c42d41b926d0798c7e5514fa14
Tobias Escobar <tobyescobar19@gmail.com>
2022-02-10 16:10:26 -0300

    Ejercicio 7 IF hecho

--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -1,8 +1,15 @@
+/*Comentarios
+Tobias Escobar
+Ejercicio 7 IF*/
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
-	
+	let edad;
+	let estadoCivil;
 
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+	estadoCivil = document.getElementById("estadoCivil").value;
 
-}//FIN DE LA FUNCIÓN
+		if(edad <= 17 && estadoCivil != "Soltero"){
+			alert("Es muy pequeño para no ser soltero.");
+		}
+}
